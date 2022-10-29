c4bf1657e99cc21e50c9462b5201ae851174e115
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-29 11:00:29 +0300

    Create UpdateGradlePlugin.hx

---
+++ source/macros/UpdateGradlePlugin.hx
@@ -0,0 +1,26 @@
+package macros;
+
+import haxe.macro.Compiler;
+import haxe.macro.Expr;
+using StringTools;
+import sys.io.File;
+import sys.FileSystem;
+
+class UpdateGradlePlugin {
+    public static macro function run(version:String):Expr.ExprOf<String> {
+        #if !display
+        var folder = '${Compiler.getOutput()}/../bin/';
+        var normalFile:String = '';
+        if (FileSystem.exists(folder + 'build.gradle')) {
+            normalFile = File.getContent(folder + 'build.gradle');
+            FileSystem.deleteFile(folder + 'build.gradle');
+            var resFile:String = normalFile.replace("::ANDROID_GRADLE_PLUGIN::", version);
+            File.saveContent(folder + 'build.gradle', resFile);
+            return macro $v{File.getContent(folder + 'build.gradle')};
+        }
+        return macro $v{"failed"};
+        #else
+        return macro $v{""};
+        #end
+    }
+}
